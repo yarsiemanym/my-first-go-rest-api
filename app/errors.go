26a64ca98120f7cd5259db928d54308c21cf3d57
Ray Saltrelli <ray@saltrelli.com>
2020-06-17 20:08:04 -0400

    Fixed typo in error message

--- app/errors.go
+++ app/errors.go
@@ -9,7 +9,7 @@ var NotFoundHandler = func(next http.Handler) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusNotFound)
-		u.Respond(w, u.Message(false, "This resources was not found on our server"))
+		u.Respond(w, u.Message(false, "This resource was not found on our server"))
 		next.ServeHTTP(w, r)
 	})
 }
